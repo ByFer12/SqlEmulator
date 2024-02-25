0a344c2ed303a66c4e51f162008cddb9d507c921
Byron Torres <ajxupbyron@gmail.com>
2024-02-24 21:47:19 -0600

    iniciando proyecto compi

---
+++ src/main/java/com/mycompany/sqlemulator/SQLemulator.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sqlemulator;
+
+import com.mycompany.sqlemulator.view.Inicio;
+
+/**
+ *
+ * @author tuxrex
+ */
+public class SQLemulator {
+public static Inicio i;
+
+    public static void main(String[] args) {
+            i=new Inicio();
+            i.setVisible(true);
+            i.setLocationRelativeTo(null);
+            
+    }
+}
